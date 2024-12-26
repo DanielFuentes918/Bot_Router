d06ddb71f9942e4efc9229fe9560797f474bcb48
Daniel Fuentes <dofuentes@unah.hn>
2024-12-26 11:44:23 -0600

    se elimino funcion extra sin utilidad en utils

--- Utils.py
+++ Utils.py
@@ -5,38 +5,29 @@ import os
 from dotenv import load_dotenv
 
 
-load_dotenv()
-def pull():
-
-    # Responder inmediatamente al webhook de GitH    
+def pull(repo_path, service):
     try:
-        # Usar un hilo para ejecutar las operaciones si es necesario (alternativa)
-        def execute_operations(repo_path, service):
-            try:
-                os.chdir(repo_path)
+        os.chdir(repo_path)
 
-                # Ejecutar git pull
-                pull_result = subprocess.run(["git", "pull"], capture_output=True, text=True)
-                print(f"Git pull stdout: {pull_result.stdout}")
-                print(f"Git pull stderr: {pull_result.stderr}")
+        # Ejecutar git pull
+        pull_result = subprocess.run(["git", "pull"], capture_output=True, text=True)
+        print(f"Git pull stdout: {pull_result.stdout}")
+        print(f"Git pull stderr: {pull_result.stderr}")
 
-                # Verificar si git pull tuvo éxito
-                if pull_result.returncode != 0:
-                    print(f"Error al ejecutar git pull: {pull_result.stderr}")
+        # Verificar si git pull tuvo éxito
+        if pull_result.returncode != 0:
+            print(f"Error al ejecutar git pull: {pull_result.stderr}")
 
-                # Reiniciar el servicio
-                restart_result = subprocess.run(["sudo", "systemctl", "restart", service], capture_output=True, text=True)
-                print(f"Service restart stdout: {restart_result.stdout}")
-                print(f"Service restart stderr: {restart_result.stderr}")
+        # Reiniciar el servicio
+        restart_result = subprocess.run(["sudo", "systemctl", "restart", service], capture_output=True, text=True)
+        print(f"Service restart stdout: {restart_result.stdout}")
+        print(f"Service restart stderr: {restart_result.stderr}")
 
-                if restart_result.returncode != 0:
-                    print(f"Error al reiniciar el servicio: {restart_result.stderr}")
-
-            except Exception as e:
-                print(f"Excepción al ejecutar operaciones: {e}")
-
-        # Ejecutar las operaciones en un hilo separado para no bloquear la respuesta
-        Thread(target=execute_operations).start()  # Usa hilos si quieres ejecutar en segundo plano
+        if restart_result.returncode != 0:
+            print(f"Error al reiniciar el servicio: {restart_result.stderr}")
 
     except Exception as e:
-        return jsonify({"status": "error", "message": f"Ocurrió un error: {str(e)}"}), 500
+        print(f"Excepción al ejecutar operaciones: {e}")
+
+# Ejecutar las operaciones en un hilo separado para no bloquear la respuesta
+Thread(target=pull).start()
