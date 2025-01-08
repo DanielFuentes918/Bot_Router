7e66ddb46c4c47e4fe007f813f27e9f0f6b461bf
Daniel Fuentes <dofuentes@unah.hn>
2025-01-08 15:52:16 -0600

    eliminar encabezados innecesarios en la función send_notification

--- app.py
+++ app.py
@@ -102,13 +102,13 @@ def send_notification():
     try:
         # Obtener los datos de la solicitud
         data = request.get_json()
-        headers = dict(request.headers)  # Copiar los encabezados
+        # headers = dict(request.headers)  # Copiar los encabezados
 
         # URL al puerto interno 5002
         forward_url = "http://127.0.0.1:5002//send_notification"
 
         # Reenviar la solicitud al puerto 5002
-        response = requests.post(forward_url, json=data, headers=headers)
+        response = requests.post(forward_url, json=data)
 
         # Devolver la respuesta del servidor interno
         return jsonify(response.json()), response.status_code
